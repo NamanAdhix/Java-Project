b0e9b609eb5b748cc3e1aea42bd09a904519111a
NamanAdhix <namanadhikari577@gmail.com>
2026-05-26 16:09:43 +0545

    3rd lab

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,32 +1,53 @@
-import java.util.Scanner;
+import java.util.*;
+interface LibraryHelper {
+    void checkBooks();
+}
 
-public class Student {
-
-    int roll;
+abstract class Student {
     String name;
+    int marks;
 
-    Student(int r, String n) {
-        roll = r;
+    Student(String n, int m) {
         name = n;
+        marks = m;
+    }
+
+    void attendClass() {
+        System.out.println(name + " attending class");
+    }
+
+    int gradeCalculator() {
+        return marks / 25;
+    }
+
+    abstract void display();
+}
+
+class StudentCEIV extends Student implements LibraryHelper {
+
+    StudentCEIV(String n, int m) {
+        super(n, m);
+    }
+
+    public void checkBooks() {
+        System.out.println("Books checked");
     }
 
     void display() {
         System.out.println("Name: " + name);
-        System.out.println("Roll: " + roll);
+        System.out.println("Marks: " + marks);
     }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+      
+        StudentCEIV s = new StudentCEIV( "Amaraj", 85);
 
-        System.out.println("Enter your name:");
-        String n = sc.nextLine();
+        s.display();
+        s.attendClass();
+        s.checkBooks();
 
-        System.out.println("Enter your roll:");
-        int r = sc.nextInt();
-
-        Student u = new Student(r, n);
-
-        u.display();
+        System.out.println("Grade: " + s.gradeCalculator());
     }
 }
